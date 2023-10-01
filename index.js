8b87e5f50ccab6301d6c8681d9a9fcb1b6f520bd
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-10-01 13:11:27 +1100

    dom added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const body = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id='victory';
+newHeader.innerHTML= 'Zeus is the champion';
